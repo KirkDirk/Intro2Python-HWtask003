bb229d45183b782e3ab16fb9cc7c9674a3450e8d
Kirk Dirk <jackalex@ya.ru>
2022-09-26 23:06:51 +0500

    add code and screenshot

--- HWtask003.py
+++ HWtask003.py
@@ -7,7 +7,16 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-x = input('Введите координаты x(≠ 0)): ')
-y = input('и y(≠ 0): ')
-
-print('Точка с координатами (', x, y, ') находится в четверти № ', quarternumber)
+x = int(input('Введите координаты x: '))
+y = int(input('и координату y: '))
+quarternumber = 1
+if x<0 and y>0: quarternumber = 2
+if x<0 and y<0: quarternumber = 3
+if x>0 and y<0: quarternumber = 4
+if x==0 and y==0: quarternumber = 0
+if x==0 and y!=0: quarternumber = -1
+if x!=0 and y==0: quarternumber = -2
+if quarternumber>0: print('Точка с координатами (', x,',', y, ') находится в четверти ', quarternumber)
+if quarternumber==-2: print('Точка лежит на оси абсцисс')
+if quarternumber==-1: print('Точка лежит на оси ординат')
+if quarternumber==0: print('Точка есть начало координат')
